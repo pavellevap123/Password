ea15732b1108aabb6bc39b570918c6d9835aa04b
Pavel Poddubotskiy <pavel.poddubotskiy@pm.by>
2022-07-31 22:14:11 +0300

    Initial Commit

---
+++ Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Pavel Poddubotskiy on 31.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
